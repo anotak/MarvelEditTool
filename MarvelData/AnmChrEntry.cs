c34d919938f554e6d82c0354e7cc8314c09c8ae4
anotak <takilgore@gmail.com>
2017-04-21 15:32:23 -0500

    anmchr stuff started

---
+++ MarvelData/AnmChrEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MarvelData
+{
+    public class AnmChrEntry : TableEntry
+    {
+        //size is naturally from list
+        public int subcount;
+        public int animTime;
+        public int unk08;
+        public int unk0C;
+        public List<AnmChrSubEntry> subEntries;
+
+        public override void SetData(byte[] newdata)
+        {
+            using (MemoryStream stream = new MemoryStream(newdata))
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    subEntries.Clear();
+                    int subcount = reader.ReadInt32();
+                    animTime = reader.ReadInt32();
+                    unk08 = reader.ReadInt32();
+                    unk0C = reader.ReadInt32();
+
+                    for (int i = 0; i < subcount; i++)
+                    {
+                        AnmChrSubEntry subentry = new AnmChrSubEntry();
+                        subentry.tableindex = reader.ReadInt32();
+                        subentry.originalPointer = reader.ReadUInt32();
+                    }
+
+                    for (int i = 0; i < subcount; i++)
+                    {
+                        subEntries[i].SetData(reader);
+                    }
+                }
+            }
+        }
+
+        public override byte[] GetData()
+        {
+            throw new Exception();
+            return null;
+        }
+    }
+}
---
+++ MarvelData/AnmChrSubEntry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MarvelData
+{
+    public class AnmChrSubEntry
+    {
+        public int tableindex;
+        public int localindex;
+        public int size;
+        public int unk08;
+        public int unk0C;
+        public uint originalPointer; // DONT SAVE THIS
+        public List<uint> subsubPointers; // DONT SAVE THIS
+        public List<byte[]> subsubEntries;
+        public List<int> subsubIndices;
+
+        public void SetData(BinaryReader reader)
+        {
+            reader.BaseStream.Seek(originalPointer, SeekOrigin.Begin);
+
+            localindex = reader.ReadInt32();
+            if (localindex != tableindex)
+            {
+                AELogger.Log(" localindex " + localindex + " != tableindex " + tableindex);
+            }
+            int subcount = reader.ReadInt32();
+            unk08 = reader.ReadInt32();
+            unk0C = reader.ReadInt32();
+            subsubEntries.Clear();
+            subsubIndices.Clear();
+            subsubPointers.Clear();
+
+            for (int i = 0; i < subcount; i++)
+            {
+                subsubPointers.Add(reader.ReadUInt32());
+                subsubIndices.Add(reader.ReadInt32());
+            }
+
+            for (int i = 0; i < subcount; i++)
+            {
+                int currentSize;
+                if (i == subcount - 1)
+                {
+                    currentSize = (int)reader.BaseStream.Length - (int)subsubPointers[i];
+                }
+                else
+                {
+                    currentSize = (int)subsubPointers[i+1] - (int)subsubPointers[i];
+                }
+                reader.BaseStream.Seek(originalPointer + subsubPointers[i], SeekOrigin.Begin);
+
+                subsubEntries.Add(reader.ReadBytes(currentSize));
+            }
+        }
+    }
+}
