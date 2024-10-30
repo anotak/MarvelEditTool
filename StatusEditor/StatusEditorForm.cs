4896a8823c1e1c007e4ec084902ed14802b4ae96
anotak <anotak@users.noreply.github.com>
2024-10-30 19:05:12 +0100

    fix ctrl-o crashing

--- StatusEditor/StatusEditorForm.cs
+++ StatusEditor/StatusEditorForm.cs
@@ -325,7 +325,7 @@ namespace StatusEditor
                     }
                 //openFile.DefaultExt = "bcm";
                 // The Filter property requires a search string after the pipe ( | )
-                SetFilter(openFile, (string)((ToolStripMenuItem)sender).Text);
+                SetFilter(openFile, (string)((ToolStripMenuItem)sender)?.Text);
                 openFile.ShowDialog();
                 if (openFile.FileNames.Length > 0)
                 {
@@ -357,6 +357,20 @@ namespace StatusEditor
 
         private static void SetFilter(OpenFileDialog openFile, string chosenFileType)
         {
+            string default_filter = "Supported Data (" +
+                    "*.ccm;*.28DD8317;*.ati;*.227A8048;*.chs;*.3C41466B;*.cba;*.3C6EA504;*.csp;*.52A8DBF6;*.cli;*.5B486CCE;*.sht;*.10BE43D4;*.cpi;*.1DF3E03E)|" +
+                    "*.ccm;*.28DD8317;*.csp;*.52A8DBF6;*.ati;*.227A8048;*.chs;*.3C41466B;*.cba;*.3C6EA504;*.cli;*.5B486CCE;*.sht;*.10BE43D4;*.cpi;*.1DF3E03E|" +
+                    "AtkInfo Files (*.ati;*.227A8048)|*.ati;*.227A8048|BaseAct Files (*.cba;*.3C6EA504)|*.cba;*.3C6EA504|" +
+                    "Cmdcombo Files (*.ccm;*.28DD8317)|*.ccm;*.28DD8317|Cmdspatk Files (*.csp;*.52A8DBF6)|*.csp;*.52A8DBF6|" +
+                    "Status Files (*.chs;*.3C41466B)|*.chs;*.3C41466B|Collision Files (*.cli;*.5B486CCE)|.cli;*.5B486CCE|" +
+                    "Shot Files (*.sht;*.10BE43D4)|*.sht;*.10BE43D4|Intro or Outro ID File (*.cpi;*.1DF3E03E)|*.cpi;*.1DF3E03E|" +
+                    "All files (*.*)|*.*";
+            if (String.IsNullOrEmpty(chosenFileType))
+            {
+                openFile.Filter = default_filter;
+                return;
+            }
+
             if (chosenFileType.Contains("ATI"))
             {
                 openFile.Filter = "AtkInfo Files (*.ati;*.227A8048)|*.ati;*.227A8048";
@@ -390,14 +404,9 @@ namespace StatusEditor
                 openFile.Filter = "Intro or Outro ID File (*.cpi;*.1DF3E03E)|*.cpi;*.1DF3E03E";
             }
             else
-                openFile.Filter = "Supported Data (" +
-                    "*.ccm;*.28DD8317;*.ati;*.227A8048;*.chs;*.3C41466B;*.cba;*.3C6EA504;*.csp;*.52A8DBF6;*.cli;*.5B486CCE;*.sht;*.10BE43D4;*.cpi;*.1DF3E03E)|" +
-                    "*.ccm;*.28DD8317;*.csp;*.52A8DBF6;*.ati;*.227A8048;*.chs;*.3C41466B;*.cba;*.3C6EA504;*.cli;*.5B486CCE;*.sht;*.10BE43D4;*.cpi;*.1DF3E03E|" +
-                    "AtkInfo Files (*.ati;*.227A8048)|*.ati;*.227A8048|BaseAct Files (*.cba;*.3C6EA504)|*.cba;*.3C6EA504|" +
-                    "Cmdcombo Files (*.ccm;*.28DD8317)|*.ccm;*.28DD8317|Cmdspatk Files (*.csp;*.52A8DBF6)|*.csp;*.52A8DBF6|" +
-                    "Status Files (*.chs;*.3C41466B)|*.chs;*.3C41466B|Collision Files (*.cli;*.5B486CCE)|.cli;*.5B486CCE|" +
-                    "Shot Files (*.sht;*.10BE43D4)|*.sht;*.10BE43D4|Intro or Outro ID File (*.cpi;*.1DF3E03E)|*.cpi;*.1DF3E03E|" +
-                    "All files (*.*)|*.*";
+            {
+                openFile.Filter = default_filter;
+            }
         }
 
         private void CloseButton_Click(object sender, EventArgs ev)
